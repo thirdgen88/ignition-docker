e31048a75dbf65587275d1fb0a805721f1b421b7
Kevin Collins <thirdgen88@gmail.com>
2019-06-14 23:02:02 -0500

    Adding support for third-party module auto-loading

--- 8.0/docker-entrypoint.sh
+++ 8.0/docker-entrypoint.sh
@@ -135,6 +135,63 @@ stop_process() {
     fi
 }
 
+# usage register_modules
+#   ie: register_modules
+register_modules() {
+    local SQLITE3=( sqlite3 /var/lib/ignition/data/db/config.idb )
+
+    # Remove Invalid Symbolic Links
+    find /var/lib/ignition/user-lib/modules -type l ! -exec test -e {} \; -exec echo "Removing invalid symlink for {}" \; -exec rm {} \;
+
+    # Establish Symbolic Links for new modules and tie into db
+    for module in /modules/*; do
+        local module_basename=$(basename "${module}")
+        local module_sourcepath=${module}
+        local module_destpath="/var/lib/ignition/user-lib/modules/${module_basename}"
+        local keytool="/usr/local/share/ignition/lib/runtime/jre-nix/bin/keytool"
+
+        if [ -h "${module_destpath}" ]; then
+            echo "Skipping Linked Module: ${module_basename}"
+            continue
+        fi
+
+        if [ -e "${module_destpath}" ]; then
+            echo "Relinking Module: ${module_basename}"
+            rm "${module_destpath}"
+        else
+            echo "Linking Module:" $(basename "${module}")
+        fi
+        ln -s "${module_sourcepath}" "${module_destpath}"
+
+        # Populate CERTIFICATES table
+        local cert_info=$( unzip -qq -c "${module_sourcepath}" certificates.p7b | $keytool -printcert -v | head -n 9 )
+        local thumbprint=$( echo "${cert_info}" | grep -A 2 "Certificate fingerprints" | grep SHA1 | cut -d : -f 2- | sed -e 's/\://g' | awk '{$1=$1;print tolower($0)}' )
+        local subject_name=$( echo "${cert_info}" | grep -A 1 "Certificate\[1\]:" | grep -Po '^Owner: CN=\K(.+)(?=, OU)' | sed -e 's/"//g' )
+        echo "  Thumbprint: ${thumbprint}"
+        echo "  Subject Name: ${subject_name}"
+        local next_certificates_id=$( "${SQLITE3[@]}" "SELECT COALESCE(MAX(CERTIFICATES_ID)+1,1) FROM CERTIFICATES" )
+        local thumbprint_already_exists=$( "${SQLITE3[@]}" "SELECT 1 FROM CERTIFICATES WHERE lower(hex(THUMBPRINT)) = '${thumbprint}'" )
+        if [ "${thumbprint_already_exists}" != "1" ]; then
+            echo "  Inserting into CERTIFICATES table as CERTIFICATES_ID=${next_certificates_id}"
+            "${SQLITE3[@]}" "INSERT INTO CERTIFICATES (CERTIFICATES_ID, THUMBPRINT, SUBJECTNAME) VALUES (${next_certificates_id}, x'${thumbprint}', '${subject_name}'); UPDATE SEQUENCES SET val=${next_certificates_id} WHERE name='CERTIFICATES_SEQ'"
+        else
+            echo "  Thumbprint already found in CERTIFICATES table, skipping INSERT"
+        fi
+
+        # Populate EULAS table
+        local next_eulas_id=$( "${SQLITE3[@]}" "SELECT COALESCE(MAX(EULAS_ID)+1,1) FROM EULAS" )
+        local license_crc32=$( unzip -qq -c "${module_sourcepath}" license.html | gzip -c | tail -c8 | od -t u4 -N 4 -A n | cut -c 2- )
+        local module_id=$( unzip -qq -c "${module_sourcepath}" module.xml | grep -oP '(?<=<id>).*(?=</id)' )
+        local module_id_already_exists=$( "${SQLITE3[@]}" "SELECT 1 FROM EULAS WHERE MODULEID='${module_id}' AND CRC=${license_crc32}" )
+        if [ "${module_id_already_exists}" != "1" ]; then
+            echo "  Inserting into EULAS table as EULAS_ID=${next_eulas_id}"
+            "${SQLITE3[@]}" "INSERT INTO EULAS (EULAS_ID, MODULEID, CRC) VALUES (${next_eulas_id}, '${module_id}', ${license_crc32}); UPDATE SEQUENCES SET val=${next_eulas_id} WHERE name='EULAS_SEQ'"
+        else
+            echo "  License EULA already found in EULAS table, skipping INSERT"
+        fi
+    done
+}
+
 # Collect additional arguments if we're running the gateway
 if [ "$1" = './ignition-gateway' ]; then
     # Examine memory constraints and apply to Java arguments
@@ -233,20 +290,29 @@ if [ "$1" = './ignition-gateway' -a ! -f "/usr/local/share/ignition/data/.docker
     echo "  GATEWAY_HTTPS_PORT: ${GATEWAY_HTTPS_PORT}"
     # echo "  GATEWAY_USESSL: ${GATEWAY_USESSL}"
 
-    # The restore will prepare the backup to be restored on the next gateway startup
-    if [ -f "/restore.gwbk" ]; then
+    # Perform Module Registration and Restore of Gateway Backup
+    if [[ (-d "/modules" && $(ls -1 /modules | wc -l) > 0) || "${GATEWAY_RESTORE_REQUIRED}" = "1" ]]; then
         sleep 5
         echo "Commissioning completed, awaiting initial gateway startup prior to restore..."
         health_check "Startup" ${IGNITION_STARTUP_DELAY:=60}
 
-        echo 'Restoring Gateway Backup...'
-        printf '\n' | ./gwcmd.sh --restore /restore.gwbk -y
-        stop_process $pid
-    else
+        # Gateway Restore
+        if [ "${GATEWAY_RESTORE_REQUIRED}" = "1" ]; then
+            echo 'Restoring Gateway Backup...'
+            printf '\n' | ./gwcmd.sh --restore /restore.gwbk -y
+            stop_process $pid
+        fi
+
+        register_modules
+    fi
+
+    if [ "${GATEWAY_RESTORE_REQUIRED}" != "1" ]; then
         stop_process $pid
     fi
 
     echo 'Starting Ignition Gateway...'
+else
+    register_modules
 fi
 
 exec "${CMD[@]}"
