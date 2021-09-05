dfc8a6a1bd894e00a4b87e47549b8b4a3dad4fcf
Kevin Collins <thirdgen88@gmail.com>
2021-09-04 22:40:56 -0500

    Minor fix for gateway auto-accept in 7.9
    When making shellcheck updates, this appeared to be logically flipped.  Should only run when the quarantine folder exists.

--- 7.9/accept-gwnetwork.sh
+++ 7.9/accept-gwnetwork.sh
@@ -7,7 +7,7 @@ QUARANTINE=/var/lib/ignition/data/certificates/gateway_network/quarantine
 echo "Beginning automatic gateway network certificate acceptance (${DELAY}s)..."
 
 for ((i=DELAY;i>0;i--)); do
-    if [[ ! -d "${QUARANTINE}" ]]; then
+    if [[ -d "${QUARANTINE}" ]]; then
         mapfile -t cert_files < <(find "${QUARANTINE}" -name '*.der' -type f)
         for cert_file in "${cert_files[@]}"; do
             cert_file_base=$(basename "${cert_file}")
