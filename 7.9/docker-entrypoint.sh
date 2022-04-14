b4a1b02f3c20eec9b3e54d90e79617e7a7e831c9
Kevin Collins <kcollins@purelinux.net>
2022-04-13 21:40:13 -0500

    Break out module and jdbc functions to scripts
    Module registration is now conducted via an external script.
    JDBC registration is updated and now conducted via an external script.
    Sequencing remains as it was within the entrypoint.

--- 7.9/docker-entrypoint.sh
+++ 7.9/docker-entrypoint.sh
@@ -112,74 +112,6 @@ stop_process() {
     fi
 }
 
-# usage register_jdbc RELINK_ENABLED DB_LOCATION
-#   ie: register_jdbc true /var/lib/ignition/data/db/config.idb
-register_jdbc() {
-    if [ ! -d "/jdbc" ]; then
-        return 0  # Silently exit if there is no /jdbc path
-    else
-        echo "init     | Searching for third-party JDBC drivers..."
-    fi
-
-    local RELINK_ENABLED="${1:-false}"
-    local DB_LOCATION="${2}"
-    local SQLITE3=( sqlite3 "${DB_LOCATION}" )
-
-    # Get List of JDBC Drivers
-    mapfile -t JDBC_CLASSNAMES < <( "${SQLITE3[@]}" "SELECT CLASSNAME FROM JDBCDRIVERS;" )
-    JDBC_CLASSPATHS=( "${JDBC_CLASSNAMES[@]/.//}" )  # replace dots with slashes for the paths
-
-    # Remove Invalid Symbolic Links
-    find "${IGNITION_INSTALL_LOCATION}/user-lib/jdbc" -type l ! -exec test -e {} \; -exec echo "init     | Removing invalid symlink for {}" \; -exec rm {} \;
-
-    # Establish Symbolic Links for new jdbc drivers and tie into db
-    for jdbc in /jdbc/*.jar; do
-        local jdbc_basename jdbc_sourcepath jdbc_destpath jdbc_listing
-        jdbc_basename=$(basename "${jdbc}")
-        jdbc_sourcepath=${jdbc}
-        jdbc_destpath="${IGNITION_INSTALL_LOCATION}/user-lib/jdbc/${jdbc_basename}"
-        
-        if [ -h "${jdbc_destpath}" ]; then
-            echo "init     | Skipping Linked JDBC Driver: ${jdbc_basename}"
-            continue
-        fi
-
-        # Determine if jdbc driver is a candidate for linking based on searching
-        # the list of existing JDBC Classname entries gathered above.
-        jdbc_listing=$(unzip -l "${jdbc}")
-        for ((i=0; i<${#JDBC_CLASSPATHS[*]}; i++)); do
-            classpath=${JDBC_CLASSPATHS[i]}
-            classname=${JDBC_CLASSNAMES[i]}
-            case ${jdbc_listing} in
-                *$classpath*)
-                jdbc_targetclassname=$classname
-                break;;
-            esac
-        done
-
-        # If we didn't find a match, ...
-        if [ -z "${jdbc_targetclassname}" ]; then
-            continue  # ... skip to next JDBC driver in path
-        fi
-
-        if [ -e "${jdbc_destpath}" ]; then
-            if [ "${RELINK_ENABLED}" != true ]; then
-                echo "init     | Skipping existing JDBC driver: ${jdbc_basename}"
-                continue
-            fi
-            echo "init     | Relinking JDBC Driver: ${jdbc_basename}"
-            rm "${jdbc_destpath}"
-        else
-            echo "init     | Linking JDBC Driver: ${jdbc_basename}"
-        fi
-        ln -s "${jdbc_sourcepath}" "${jdbc_destpath}"
-
-        # Update JDBCDRIVERS table
-        echo "init     |   Updating JDBCDRIVERS table for classname ${jdbc_targetclassname}"
-        "${SQLITE3[@]}" "UPDATE JDBCDRIVERS SET JARFILE='${jdbc_basename}' WHERE CLASSNAME='${jdbc_targetclassname}'"
-    done
-}
-
 # usage enable_disable_modules MODULES_ENABLED
 #   ie: enable_disable_modules vision,opc-ua,sql-bridge
 enable_disable_modules() {
@@ -255,78 +187,6 @@ enable_disable_modules() {
 		fi
 	done
 	echo
-}
-
-# usage register_modules RELINK_ENABLED DB_LOCATION
-#   ie: register_modules true /var/lib/ignition/data/db/config.idb
-register_modules() {
-    if [ ! -d "/modules" ]; then
-        return 0  # Silently exit if there is no /modules path
-    else
-        echo "init     | Searching for third-party modules..."
-    fi
-
-    local RELINK_ENABLED="${1:-false}"
-    local DB_LOCATION="${2}"
-    local SQLITE3=( sqlite3 "${DB_LOCATION}" )
-
-    # Remove Invalid Symbolic Links
-    find "${IGNITION_INSTALL_LOCATION}/user-lib/modules" -type l ! -exec test -e {} \; -exec echo "Removing invalid symlink for {}" \; -exec rm {} \;
-
-    # Establish Symbolic Links for new modules and tie into db
-    for module in /modules/*.modl; do
-        local module_basename module_sourcepath module_destpath keytool
-        module_basename=$(basename "${module}")
-        module_sourcepath=${module}
-        module_destpath="${IGNITION_INSTALL_LOCATION}/user-lib/modules/${module_basename}"
-        keytool=$(which keytool)
-
-        if [ -h "${module_destpath}" ]; then
-            echo "init     | Skipping Linked Module: ${module_basename}"
-            continue
-        fi
-
-        if [ -e "${module_destpath}" ]; then
-            if [ "${RELINK_ENABLED}" != true ]; then
-                echo "init     | Skipping existing module: ${module_basename}"
-                continue
-            fi
-            echo "init     | Relinking Module: ${module_basename}"
-            rm "${module_destpath}"
-        else
-            echo "init     | Linking Module: ${module_basename}"
-        fi
-        ln -s "${module_sourcepath}" "${module_destpath}"
-
-        # Populate CERTIFICATES table
-        local cert_info thumbprint subject_name next_certificates_id thumbprint_already_exists
-        cert_info=$( unzip -qq -c "${module_sourcepath}" certificates.p7b | $keytool -printcert -v | head -n 9 )
-        thumbprint=$( echo "${cert_info}" | grep -A 2 "Certificate fingerprints" | grep SHA1 | cut -d : -f 2- | sed -e 's/\://g' | awk '{$1=$1;print tolower($0)}' )
-        subject_name=$( echo "${cert_info}" | grep -A 1 "Certificate\[1\]:" | grep -Po '^Owner: CN=\K(.+)(?=, OU)' | sed -e 's/"//g' )
-        echo "init     |   Thumbprint: ${thumbprint}"
-        echo "init     |   Subject Name: ${subject_name}"
-        next_certificates_id=$( "${SQLITE3[@]}" "SELECT COALESCE(MAX(CERTIFICATES_ID)+1,1) FROM CERTIFICATES" )
-        thumbprint_already_exists=$( "${SQLITE3[@]}" "SELECT 1 FROM CERTIFICATES WHERE lower(hex(THUMBPRINT)) = '${thumbprint}'" )
-        if [ "${thumbprint_already_exists}" != "1" ]; then
-            echo "init     |   Accepting Certificate as CERTIFICATES_ID=${next_certificates_id}"
-            "${SQLITE3[@]}" "INSERT INTO CERTIFICATES (CERTIFICATES_ID, THUMBPRINT, SUBJECTNAME) VALUES (${next_certificates_id}, x'${thumbprint}', '${subject_name}'); UPDATE SEQUENCES SET val=${next_certificates_id} WHERE name='CERTIFICATES_SEQ'"
-        else
-            echo "init     |   Thumbprint already found in CERTIFICATES table, skipping INSERT"
-        fi
-
-        # Populate EULAS table
-        local next_eulas_id license_crc32 module_id module_id_already_exists
-        next_eulas_id=$( "${SQLITE3[@]}" "SELECT COALESCE(MAX(EULAS_ID)+1,1) FROM EULAS" )
-        license_crc32=$( unzip -qq -c "${module_sourcepath}" license.html | gzip -c | tail -c8 | od -t u4 -N 4 -A n | cut -c 2- )
-        module_id=$( unzip -qq -c "${module_sourcepath}" module.xml | grep -oP '(?<=<id>).*(?=</id)' )
-        module_id_already_exists=$( "${SQLITE3[@]}" "SELECT 1 FROM EULAS WHERE MODULEID='${module_id}' AND CRC=${license_crc32}" )
-        if [ "${module_id_already_exists}" != "1" ]; then
-            echo "init     |   Accepting License on your behalf as EULAS_ID=${next_eulas_id}"
-            "${SQLITE3[@]}" "INSERT INTO EULAS (EULAS_ID, MODULEID, CRC) VALUES (${next_eulas_id}, '${module_id}', ${license_crc32}); UPDATE SEQUENCES SET val=${next_eulas_id} WHERE name='EULAS_SEQ'"
-        else
-            echo "init     |   License EULA already found in EULAS table, skipping INSERT"
-        fi
-    done
 }
 
 # usage: compare_versions IMAGE_VERSION VOLUME_VERSION
@@ -561,10 +421,10 @@ if [ "$1" = './ignition-gateway' ]; then
     fi
 
     # Link Additional Modules and prepare Ignition database
-    register_modules "${GATEWAY_MODULE_RELINK}" "${IGNITION_INSTALL_LOCATION}/data/db/config.idb"
+    register-modules.sh "${GATEWAY_MODULE_RELINK}" "${IGNITION_INSTALL_LOCATION}/data/db/config.idb"
 
     # Link Additional JDBC Drivers and prepare Ignition database
-    register_jdbc "${GATEWAY_JDBC_RELINK}" "${IGNITION_INSTALL_LOCATION}/data/db/config.idb"
+    register-jdbc.sh "${GATEWAY_JDBC_RELINK}" "${IGNITION_INSTALL_LOCATION}/data/db/config.idb"
     
     # Perform module enablement/disablement
     enable_disable_modules "${GATEWAY_MODULES_ENABLED}"
---
+++ 7.9/register-modules.sh
@@ -0,0 +1,88 @@
+#!/usr/bin/env bash
+set -euo pipefail
+shopt -s nullglob
+shopt -s inherit_errexit
+
+# usage register-modules.sh RELINK_ENABLED DB_LOCATION
+#   ie: register-modules.sh true /var/lib/ignition/data/db/config.idb
+RELINK_ENABLED="${1:-false}"
+DB_LOCATION="${2}"
+DB_FILE=$(basename "${DB_LOCATION}")
+
+function main() {
+    if [ ! -d "/modules" ]; then
+        return 0  # Silently exit if there is no /modules path
+    elif [ ! -f "${DB_LOCATION}" ]; then
+        echo "init     | WARNING: ${DB_FILE} not found, skipping module registration"
+        return 0
+    fi
+
+    register_modules
+}
+
+function register_modules() {
+    local SQLITE3=( sqlite3 "${DB_LOCATION}" )
+
+    echo "init     | Searching for third-party modules..."
+
+    # Remove Invalid Symbolic Links
+    find "${IGNITION_INSTALL_LOCATION}/user-lib/modules" -type l ! -exec test -e {} \; -exec echo "init     | Removing invalid symlink for {}" \; -exec rm {} \;
+
+    # Establish Symbolic Links for new modules and tie into db
+    for module in /modules/*.modl; do
+        local module_basename keytool module_sourcepath module_destpath
+        module_basename=$(basename "${module}")
+        module_sourcepath=${module}
+        module_destpath="${IGNITION_INSTALL_LOCATION}/user-lib/modules/${module_basename}"
+        keytool=$(which keytool)
+
+        if [ -h "${module_destpath}" ]; then
+            echo "init     | Skipping Linked Module: ${module_basename}"
+            continue
+        fi
+
+        if [ -e "${module_destpath}" ]; then
+            if [ "${RELINK_ENABLED}" != true ]; then
+                echo "init     | Skipping existing module: ${module_basename}"
+                continue
+            fi
+            echo "init     | Relinking Module: ${module_basename}"
+            rm "${module_destpath}"
+        else
+            echo "init     | Linking Module: ${module_basename}"
+        fi
+        ln -s "${module_sourcepath}" "${module_destpath}"
+
+        # Populate CERTIFICATES table
+        local cert_info subject_name thumbprint next_certificates_id thumbprint_already_exists
+        cert_info=$( unzip -qq -c "${module_sourcepath}" certificates.p7b | $keytool -printcert -v | head -n 9 ) 
+        thumbprint=$( echo "${cert_info}" | grep -A 2 "Certificate fingerprints" | grep SHA1 | cut -d : -f 2- | sed -e 's/\://g' | awk '{$1=$1;print tolower($0)}' ) 
+        echo "init     |  Thumbprint: ${thumbprint}"
+        subject_name=$( echo "${cert_info}" | grep -A 1 "Certificate\[1\]:" | grep -Po '^Owner: CN="?\K(.+?)(?="?, (OU?|L|ST|C))' | sed -e 's/"//g' )
+        echo "init     |  Subject Name: ${subject_name}"
+        next_certificates_id=$( "${SQLITE3[@]}" "SELECT COALESCE(MAX(CERTIFICATES_ID)+1,1) FROM CERTIFICATES" ) 
+        thumbprint_already_exists=$( "${SQLITE3[@]}" "SELECT 1 FROM CERTIFICATES WHERE lower(hex(THUMBPRINT)) = '${thumbprint}'" )
+        if [ "${thumbprint_already_exists}" != "1" ]; then
+            echo "init     |  Accepting Certificate as CERTIFICATES_ID=${next_certificates_id}"
+            "${SQLITE3[@]}" "INSERT INTO CERTIFICATES (CERTIFICATES_ID, THUMBPRINT, SUBJECTNAME) VALUES (${next_certificates_id}, x'${thumbprint}', '${subject_name}'); UPDATE SEQUENCES SET val=${next_certificates_id} WHERE name='CERTIFICATES_SEQ'"
+        else
+            echo "init     |  Thumbprint already found in CERTIFICATES table, skipping INSERT"
+        fi
+
+        # Populate EULAS table
+        local next_eulas_id license_crc32 module_id module_id_already_exists
+        next_eulas_id=$( "${SQLITE3[@]}" "SELECT COALESCE(MAX(EULAS_ID)+1,1) FROM EULAS" ) 
+        license_filename=$( unzip -qq -c "${module_sourcepath}" module.xml | grep -oP '(?<=<license>).*(?=</license)' )
+        license_crc32=$( unzip -qq -c "${module_sourcepath}" "${license_filename}" | gzip -c | tail -c8 | od -t u4 -N 4 -A n | cut -c 2- ) 
+        module_id=$( unzip -qq -c "${module_sourcepath}" module.xml | grep -oP '(?<=<id>).*(?=</id)' ) 
+        module_id_already_exists=$( "${SQLITE3[@]}" "SELECT 1 FROM EULAS WHERE MODULEID='${module_id}' AND CRC=${license_crc32}" )
+        if [ "${module_id_already_exists}" != "1" ]; then
+            echo "init     |  Accepting License on your behalf as EULAS_ID=${next_eulas_id}"
+            "${SQLITE3[@]}" "INSERT INTO EULAS (EULAS_ID, MODULEID, CRC) VALUES (${next_eulas_id}, '${module_id}', ${license_crc32}); UPDATE SEQUENCES SET val=${next_eulas_id} WHERE name='EULAS_SEQ'"
+        else
+            echo "init     |  License EULA already found in EULAS table, skipping INSERT"
+        fi
+    done
+}
+
+main
